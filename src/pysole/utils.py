57032ec90f19aee80863daac1863b527ac7ca485
Tzur Soffer <tzur.soffer@gmail.com>
2025-10-15 19:45:04 -0700

    added error handling for stdPrint

--- src/pysole/utils.py
+++ src/pysole/utils.py
@@ -6,5 +6,8 @@ themesPath = os.path.join(os.path.dirname(__file__), "themes.json")
 
 def stdPrint(text):
     """Print text to the terminal."""
-    sys.__stdout__.write(f"{text}\n")
-    sys.__stdout__.flush()
+    try:
+        sys.__stdout__.write(f"{text}\n")
+        sys.__stdout__.flush()
+    except:
+        pass
